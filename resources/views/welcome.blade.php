aeb4341aacba53aa22de544318af278e46230404
papooni <vascosousa@MacBook-Pro-2.local>
2016-11-18 15:42:42 +0000

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" href="http://ipoporto.pt/wp-content/themes/IPO/ipoporto_ficon.png" />
     <title>Correio IPO</title>
 
     <!-- Fonts -->
