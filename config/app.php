7db88fb700f18e5fd0cc9b24f1d7f3c4744babe0
papooni <vascosousa@MacBook-Pro-2.local>
2016-11-21 16:29:15 +0000

    11

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'IPO - Gestão de Correio Interno',
 
     /*
     |--------------------------------------------------------------------------
