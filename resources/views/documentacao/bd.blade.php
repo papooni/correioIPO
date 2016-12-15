575df655b1f53070e50fa1023ef1b6686b4d353d
papooni <vascosousa@MacBook-Pro-2.local>
2016-12-15 09:16:30 +0000

    img doc

--- resources/views/documentacao/bd.blade.php
+++ resources/views/documentacao/bd.blade.php
@@ -1,5 +1,5 @@
 @extends('documentacao.docdashboard')
 @section('page_heading', 'Base de Dados')
 @section('section')
-<img src="docs/db.png" width="50%" class="center-block">
+<img src="img/docs/db.png" width="50%" class="center-block">
 @stop
