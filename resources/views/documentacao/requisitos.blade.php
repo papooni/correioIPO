840167439bbddbc2c7e38da2cb3a8609007277db
papooni <vascosousa@MacBook-Pro-2.local>
2016-12-29 11:43:16 +0000

    doc requisitos

--- resources/views/documentacao/requisitos.blade.php
+++ resources/views/documentacao/requisitos.blade.php
@@ -1,5 +1,29 @@
 @extends('documentacao.docdashboard')
 @section('page_heading', 'Requisitos')
 @section('section')
+<div class="form-group">
+    A versão da framework Laravel utilizada é a 5.3 e tem alguns requisitos de sistema:
+   <ul>
+       <li>Versão de PHP >= 5.6.4
+       </li>
+       <li>OpenSSL Php Extension
+       </li>
+       <li>PDO PHP Extension
+       </li>
+       <li>Mbstring PHP Extension
+       </li>
+       <li>Tokenizer PHP Extension
+       </li>
+       <li>XML PHP Extension
+       </li>
 
+   </ul>
+
+
+
+
+
+
+
+</div>
 @stop
